ca538a8ca13b13148bcda03de4ef35f347e6e9f6
Евгений Силаев <110228261+Silavez@users.noreply.github.com>
2022-12-07 17:55:43 +0300

    Глава 4. Условный оператор

---
+++ Stepik/Lesson_4/4.1_Naimenshee iz 4etirioh.py
@@ -0,0 +1,10 @@
+#Напишите программу, которая определяет наименьшее из четырёх чисел.
+
+a, b, c, d = int(input()), int(input()), int(input()), int(input())
+if a > b:
+    a = b
+if c > d:
+    c = d
+if a > c:
+    a = c
+print(a)
